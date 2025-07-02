6ffe2a4ecd6de6904d3c26106d2def662fb9438b
Maxime Fagnan <71909089+MaximeFagnan@users.noreply.github.com>
2025-07-02 13:36:01 -0400

    runtime info for snake enumerator

--- polyEnum_Jensen/python_snakeEnum/Tests/OEIS_comparator.py
+++ polyEnum_Jensen/python_snakeEnum/Tests/OEIS_comparator.py
@@ -12,8 +12,8 @@ from enumerator import PolyominoEnumerator
 # I added the empty snake polyomino to this list
 counts = [1,1,2,6,14,34,82,198,470,1122,2662,6334,14970,35506,83734,198086,466314,1100818,2587634,6097830,14316402,33687146,79008870,185677006,435098774,1021404998,2391646494,5609151738,13125214770,30757286802,71928506630]
 
-# n = 19 program runtime: 223s
-for n in [11]:
+
+for n in [15,17]:
 
     poly_enum = PolyominoEnumerator(n)
     start_time = time.time()
@@ -39,3 +39,17 @@ for n in [11]:
     #         print(f"{i}: {coeff} COUNTED CORRECTLY")
     #     else:
     #         print(f"{i}: count={counts}, coeff={coeff} COUNTED INCORRECTLY")
+
+
+
+# n = 15
+# program runtime: 7.289050340652466
+# 1x^0 + 1x^1 + 2x^2 + 6x^3 + 14x^4 + 34x^5 + 82x^6 + 198x^7 + 470x^8 + 1122x^9 + 2662x^10 + 6334x^11 + 14970x^12 + 35506x^13 + 83734x^14 + 198086x^15
+# Verification: a_n coefficients match OEIS coefficients at https://oeis.org/A182644/list
+
+# n = 17
+# program runtime: 41.406007528305054
+# 1x^0 + 1x^1 + 2x^2 + 6x^3 + 14x^4 + 34x^5 + 82x^6 + 198x^7 + 470x^8 + 1122x^9 + 2662x^10 + 6334x^11 + 14970x^12 + 35506x^13 + 83734x^14 + 198086x^15 + 466314x^16 + 1100818x^17
+# Verification: a_n coefficients match OEIS coefficients at https://oeis.org/A182644/list
+
+# n = 19 program runtime: 223s
