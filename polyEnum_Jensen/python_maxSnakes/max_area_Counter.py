8086ddf8c44ab94d589074306946859c4692de03
Maxime Fagnan <71909089+MaximeFagnan@users.noreply.github.com>
2025-07-02 03:03:34 -0400

    maxSnakes: transitions table and max_areaCounter (generating founction)

---
+++ polyEnum_Jensen/python_maxSnakes/max_area_Counter.py
@@ -0,0 +1,30 @@
+class max_area_Counter:
+    def __init__(self, max_area = None, nb_configs = None):
+        self.max_area = 0 if max_area is None else max_area
+        self.nb_configs = 0 if nb_configs is None else nb_configs
+        
+    def __add__(self, other):
+        assert isinstance(other,max_area_Counter)
+
+        if self.max_area > other.max_area:
+            return max_area_Counter(self.max_area, self.nb_configs)
+        elif self.max_area < other.max_area:
+            return max_area_Counter(other.max_area, other.nb_configs)
+        else:
+            return max_area_Counter(self.max_area, self.nb_configs + other.nb_configs)
+
+    def increase_max_area(self):
+        self.max_area += 1
+
+    def __repr__(self):
+        return f"Max area: {self.max_area} \nNumber of configurations: {self.nb_configs}"
+
+    def clone(self):
+        return max_area_Counter(self.max_area,self.nb_configs)
+    
+    # # No need to verify if empty?
+    # def is_empty(self):
+    #     if len(self.terms) == 0: 
+    #         return True
+    #     else:
+    #         return False
